e30243b1886efc86cbe9f0d643c3490639b04b5e
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-29 08:41:07 -0300

    feat(server/handlers): app error - completed

---
+++ server/src/handlers/error/AppError.ts
@@ -0,0 +1,35 @@
+import { getMessage } from "@helpers/translatedMessagesControl";
+import { HttpStatus } from "@http/utils/HttpStatus";
+
+class AppError<T = any> extends Error {
+  public readonly message: string;
+
+  public readonly statusCode: HttpStatus;
+
+  public readonly content?: T;
+
+  constructor(
+    statusCode: keyof typeof HttpStatus,
+    message: string,
+    content?: T
+  ) {
+    super();
+    this.message = message;
+    this.statusCode = HttpStatus[statusCode];
+    this.content = content;
+  }
+
+  public static getErrorStatusCode(error: any): HttpStatus {
+    return error instanceof AppError
+      ? error.statusCode
+      : HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+
+  public static getErrorMessage(error: any): string {
+    return error instanceof AppError
+      ? error.message
+      : getMessage("ErrorGenericUnknown");
+  }
+}
+
+export { AppError };
