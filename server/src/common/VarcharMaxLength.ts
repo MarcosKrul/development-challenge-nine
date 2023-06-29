ad51f4f30eef6d5160f270127b0735f3ec0d85d0
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-29 09:09:30 -0300

    feat(server/common): varchar max length

---
+++ server/src/common/VarcharMaxLength.ts
@@ -0,0 +1,19 @@
+class VarcharMaxLength {
+  public static readonly USER_NAME: number = 100;
+
+  public static readonly USER_EMAIL: number = 255;
+
+  public static readonly ADDRESS_ZIP_CODE: number = 8;
+
+  public static readonly ADDRESS_STATE: number = 100;
+
+  public static readonly ADDRESS_CITY: number = 100;
+
+  public static readonly ADDRESS_DISTRICT: number = 100;
+
+  public static readonly ADDRESS_PUBLIC_AREA: number = 255;
+
+  public static readonly ADDRESS_COMPLEMENT: number = 255;
+}
+
+export { VarcharMaxLength };
