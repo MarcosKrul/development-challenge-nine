7757eae16b9a6b39d0f01653aebf7915983f75b5
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-29 09:49:01 -0300

    feat(server/http): patient routes - list

---
+++ server/src/infra/http/controllers/PatientController.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import { ListPatientsService } from "src/application/services/patient";
+
+import { container } from "@containers/container";
+import { ListPatientsResponseModel } from "@dtos/patient/ListPatientsResponseModel";
+import { getMessage } from "@helpers/translatedMessagesControl";
+import { IPaginationResponse } from "@http/models/IPaginationResponse";
+import { IResponseMessage } from "@http/models/IResponseMessage";
+import { HttpStatus } from "@http/utils/HttpStatus";
+
+class PatientController {
+  public async list(
+    req: Request,
+    res: Response<
+      IResponseMessage<IPaginationResponse<ListPatientsResponseModel>>
+    >,
+    next: NextFunction
+  ): Promise<void> {
+    const { size, page } = req.query;
+
+    const service = container.resolve(ListPatientsService);
+
+    const result = await service.execute({
+      page,
+      size,
+    });
+
+    res.status(HttpStatus.OK).json({
+      success: true,
+      content: result,
+      message: getMessage("SuccessGeneric"),
+    });
+
+    return next();
+  }
+}
+
+export { PatientController };
--- server/src/infra/http/routes/patient.routes.ts
+++ server/src/infra/http/routes/patient.routes.ts
@@ -1,5 +1,21 @@
 import { Router } from "express";
 
+import { PatientController } from "@http/controllers/PatientController";
+import { HandleUrlPatternMatchMiddleware } from "@middlewares/HandleUrlPatternMatchMiddleware";
+import { LogMiddleware } from "@middlewares/LogMiddleware";
+
 const routes = Router();
+const controller = new PatientController();
+const logMiddleware = new LogMiddleware();
+
+const handleUrlPatternMatchMiddleware = new HandleUrlPatternMatchMiddleware();
+
+routes.get(
+  "/",
+  handleUrlPatternMatchMiddleware.skipIfHasUrlMatched,
+  logMiddleware.routeStart,
+  controller.list,
+  handleUrlPatternMatchMiddleware.setHasUrlMatched()
+);
 
 export { routes };
