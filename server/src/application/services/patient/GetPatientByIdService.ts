540674daefc62797bf9fd0b0d1565670423c1013
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-29 19:42:28 -0300

    feat(server/services): get patient by id - completed

--- server/src/application/services/patient/GetPatientByIdService.ts
+++ server/src/application/services/patient/GetPatientByIdService.ts
@@ -1,16 +1,60 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
 
+import { IAddressRepository } from "@database/repositories/address";
+import { IPatientRepository } from "@database/repositories/patient";
+import { transaction } from "@database/transaction";
 import { GetPatientByIdRequestModel } from "@dtos/patient/GetPatientByIdRequestModel";
 import { GetPatientByIdResponseModel } from "@dtos/patient/GetPatientByIdResponseModel";
+import { AppError } from "@handlers/error/AppError";
+import { stringIsNullOrEmpty } from "@helpers/stringIsNullOrEmpty";
+import { getMessage } from "@helpers/translatedMessagesControl";
+import { IMaskProvider } from "@providers/mask";
+import { IUniqueIdentifierProvider } from "@providers/uniqueIdentifier";
 
 @injectable()
 class GetPatientByIdService {
-  public async execute(
-    obj: GetPatientByIdRequestModel
-  ): Promise<GetPatientByIdResponseModel> {
-    console.log(obj);
+  constructor(
+    @inject("UniqueIdentifierProvider")
+    private uniqueIdentifierProvider: IUniqueIdentifierProvider,
+    @inject("PatientRepository")
+    private patientRepository: IPatientRepository,
+    @inject("MaskProvider")
+    private maskProvider: IMaskProvider,
+    @inject("AddressRepository")
+    private addressRepository: IAddressRepository
+  ) {}
 
-    return {} as any;
+  public async execute({
+    patientId,
+  }: GetPatientByIdRequestModel): Promise<GetPatientByIdResponseModel> {
+    if (stringIsNullOrEmpty(patientId))
+      throw new AppError("BAD_REQUEST", getMessage("ErrorPatientIdRequired"));
+
+    if (!this.uniqueIdentifierProvider.isValid(patientId))
+      throw new AppError("BAD_REQUEST", getMessage("ErrorPatientIdInvalid"));
+
+    const [hasPatient, hasAddress] = await transaction([
+      this.patientRepository.getById({ patientId }),
+      this.addressRepository.getByPatientId({ patientId }),
+    ]);
+
+    if (!hasPatient || !hasAddress)
+      throw new AppError("NOT_FOUND", getMessage("ErrorPatientNotFound"));
+
+    return {
+      id: hasPatient.id,
+      email: hasPatient.email,
+      name: hasPatient.name,
+      birthDate: this.maskProvider.date(hasPatient.birthDate),
+      address: {
+        city: hasAddress.city,
+        complement: hasAddress.complement,
+        district: hasAddress.district,
+        publicArea: hasAddress.publicArea,
+        state: hasAddress.state,
+        zipCode: this.maskProvider.zipCode(hasAddress.zipCode),
+      },
+    };
   }
 }
 
--- server/src/infra/dtos/patient/GetPatientByIdResponseModel.ts
+++ server/src/infra/dtos/patient/GetPatientByIdResponseModel.ts
@@ -1,5 +1,16 @@
-import { ListPatientsResponseModel } from "./ListPatientsResponseModel";
-
-type GetPatientByIdResponseModel = ListPatientsResponseModel;
+type GetPatientByIdResponseModel = {
+  id: string;
+  name: string;
+  email: string;
+  birthDate: string;
+  address: {
+    city: string;
+    district: string;
+    publicArea: string;
+    state: string;
+    zipCode: string;
+    complement: string | null;
+  };
+};
 
 export { GetPatientByIdResponseModel };
