500d2ebd914e26c4419dc76bdbd9fe784775f2e2
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-29 20:56:46 -0300

    feat(client/infra): config i18n

---
+++ client/src/config/i18n.ts
@@ -0,0 +1,17 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import ptTranslation from '../../i18n_translations/pt-br.json';
+
+const resources = {
+  pt: { translation: ptTranslation },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: 'pt',
+  fallbackLng: 'pt',
+  interpolation: { escapeValue: false },
+});
+
+export default i18n;
