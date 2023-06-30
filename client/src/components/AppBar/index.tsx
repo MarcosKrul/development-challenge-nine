1a5925b033527e6f56039b946429ee3dd636e50b
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-30 15:56:09 -0300

    feat(client/components): appbar, side bar & header - material ui

---
+++ client/src/components/AppBar/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { styled } from '@mui/material/styles';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import constants from '@global/constants';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
+}
+
+const DrawerHeader = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: constants.SIDE_BAR_WIDTH,
+    width: `calc(100% - ${constants.SIDE_BAR_WIDTH}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+interface IAppBarProps {
+  open: boolean;
+  handleDrawerOpen: () => void;
+}
+
+const AppBar = ({ handleDrawerOpen, open }: IAppBarProps) => {
+  return (
+    <DrawerHeader position="fixed" open={open}>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          edge="start"
+          sx={{
+            marginRight: 5,
+            ...(open && { display: 'none' }),
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" noWrap component="div">
+          Mini variant drawer
+        </Typography>
+      </Toolbar>
+    </DrawerHeader>
+  );
+};
+
+export { AppBar };
---
+++ client/src/components/SideBar/index.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { styled, Theme, CSSObject } from '@mui/material/styles';
+import MuiDrawer from '@mui/material/Drawer';
+import constants from '@global/constants';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import { NavLink } from 'react-router-dom';
+import { SideBarHeader } from '@components/SideBarHeader';
+import { routes } from '@global/routes';
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: constants.SIDE_BAR_WIDTH,
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: 'hidden',
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up('sm')]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  width: constants.SIDE_BAR_WIDTH,
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  ...(open && {
+    ...openedMixin(theme),
+    '& .MuiDrawer-paper': openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    '& .MuiDrawer-paper': closedMixin(theme),
+  }),
+}));
+
+interface ISideBarProps {
+  open: boolean;
+  handleDrawerClose: () => void;
+  theme: Theme;
+}
+
+const SideBar = ({ open, handleDrawerClose, theme }: ISideBarProps) => {
+  return (
+    <Drawer variant="permanent" open={open}>
+      <SideBarHeader>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'rtl' ? (
+            <ChevronRightIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
+        </IconButton>
+      </SideBarHeader>
+      <Divider />
+      <List>
+        {routes.map(({ label, path, icon }) => (
+          <NavLink to={path} key={label}>
+            <ListItem key={label} disablePadding sx={{ display: 'block' }}>
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  {icon()}
+                </ListItemIcon>
+                <ListItemText primary={label} sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+        ))}
+      </List>
+    </Drawer>
+  );
+};
+
+export { SideBar };
