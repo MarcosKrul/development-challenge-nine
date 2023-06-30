8353c7cc35ab822d35772026483aae5c8c0ec381
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-06-30 15:58:16 -0300

    feat(client/route): fix routes & main page

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Outlet } from 'react-router-dom';
+import { AppBar } from '@components/AppBar';
+import { SideBarHeader } from '@components/SideBarHeader';
+import { SideBar } from '@components/SideBar';
 
-const Index = () => {
-  return <h1>Index</h1>;
+const Main = () => {
+  const theme = useTheme();
+  const [open, setOpen] = useState(true);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar open={open} handleDrawerOpen={handleDrawerOpen} />
+      <SideBar
+        open={open}
+        theme={theme}
+        handleDrawerClose={handleDrawerClose}
+      />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <SideBarHeader />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
-export { Index };
+export { Main };
--- client/src/routes/index.routes.tsx
+++ client/src/routes/index.routes.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { NotFound } from '@pages/NotFound';
+import { Main } from '@pages/Main';
 import { HomeRoutes } from './home.routes';
 import { PatientRoutes } from './patients.routes';
-import { NotFound } from '@pages/NotFound';
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home/*" element={<HomeRoutes />}></Route>
-        <Route path="/patients/*" element={<PatientRoutes />}></Route>
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Main />}>
+          <Route path="/home/*" element={<HomeRoutes />} />
+          <Route path="/patients/*" element={<PatientRoutes />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
