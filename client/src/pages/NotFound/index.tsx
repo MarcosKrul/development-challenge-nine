bba01689a936fe6a16a3caba7805ebcc104973e9
Marcos Renan Krul <marcoskrul2k@gmail.com>
2023-07-01 14:26:35 -0300

    feat(client/pages): not found completed

--- client/src/pages/NotFound/index.tsx
+++ client/src/pages/NotFound/index.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import {
+  Container,
+  LogoAndMain,
+  LogoContainer,
+  MainText,
+  MainTextContainer,
+  MainTitle,
+  SecondarySection,
+  SecondaryText,
+  StyledButton,
+} from './styles';
+import logo from '@assets/logo-medcloud.png';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const NotFound = () => {
-  return <h1>not found</h1>;
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+
+  return (
+    <Container>
+      <LogoAndMain>
+        <LogoContainer src={logo} />
+        <MainTextContainer>
+          <MainTitle>404</MainTitle>
+          <MainText>{t('TEXT_NOT_FOUND_PAGE_TITLE')}</MainText>
+        </MainTextContainer>
+      </LogoAndMain>
+
+      <SecondarySection>
+        <SecondaryText>{t('TEXT_GO_HOME')}</SecondaryText>
+        <StyledButton onClick={() => navigate('/')}>
+          {t('BUTTON_GO_HOME')}
+        </StyledButton>
+      </SecondarySection>
+    </Container>
+  );
 };
 
 export { NotFound };
